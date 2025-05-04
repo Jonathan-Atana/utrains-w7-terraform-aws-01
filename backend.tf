aeed73a9b5e5f8cdf659bbb9b1345774c40a7545
Jonathan <atanajonathan5@gmail.com>
2025-05-04 23:03:24 +0100

    Update backend.tf: change bucket name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "ja-w7-terraform-state-bucket"
-    key          = "dev/terraform.tfstate"
+    bucket       = "terraform-state-bucket-ja"
+    key          = "dev/utrains-w7-terraform/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
   }
